24df8156cf7582dd08f3a3bb14a4062ace9a5873
jAyAceSy <celizrick29@gmail.com>
2026-08-03 09:23:32 +0800

    Add files via upload
    update dashboard

---
+++ App.tsx
@@ -0,0 +1,125 @@
+import { Routes, Route } from 'react-router-dom'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import NewTransfer from './pages/NewTransfer'
+import TransferHistory from './pages/TransferHistory'
+import TransferDetail from './pages/TransferDetail'
+import Products from './pages/Products'
+import Users from './pages/Users'
+import Reports from './pages/Reports'
+import Search from './pages/Search'
+import TransferImport from './pages/TransferImport'
+import ReceiveTransfer from './pages/ReceiveTransfer'
+import VarianceReview from './pages/VarianceReview'
+import PalletLabelPrinting from './pages/PalletLabelPrinting'
+import NotFound from './pages/NotFound'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/transfers/new"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_staff']}>
+            <NewTransfer />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/transfers"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_staff']}>
+            <TransferHistory />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/transfers/:id"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_staff']}>
+            <TransferDetail />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute adminOnly>
+            <Products />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute adminOnly>
+            <Users />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/reports"
+        element={
+          <ProtectedRoute adminOnly>
+            <Reports />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Transfer Barcode Receiving feature */}
+      <Route
+        path="/transfer-management"
+        element={
+          <ProtectedRoute allowedRoles={['production']}>
+            <TransferImport />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/receive-transfer"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_staff']}>
+            <ReceiveTransfer />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/variance-review"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_officer']}>
+            <VarianceReview />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/label-printing"
+        element={
+          <ProtectedRoute allowedRoles={['warehouse_officer']}>
+            <PalletLabelPrinting />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
